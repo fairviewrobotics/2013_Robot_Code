b7402bcac1982d78b3867a9ccd9c691e2d7f3e37
unknown <pixelgel@gmail.com>
2013-01-15 17:24:40 -0700

    Replaced the template with a stripped version.

---
+++ Robot.cpp
@@ -0,0 +1,35 @@
+#include "WPILib.h"
+
+class Robot : public SimpleRobot {
+
+public:
+	Robot(void) {
+		puts("I'm a robot");
+	}
+
+	void Autonomous(void) {
+		puts("I'm autonomous");
+	}
+
+	void OperatorControl(void) {
+		puts("I'm controlled");
+	}
+	
+	void Disabled(void) {
+		puts("I'm disabled");
+	}
+	
+	void Test() {
+
+	}
+};
+
+RobotBase *FRC_userClassFactory(void) {
+	return new RobotDemo();
+}
+extern "C" {
+	int FRC_UserProgram_StartupLibraryInit(void) {
+		RobotBase::startRobotTask((FUNCPTR)FRC_userClassFactory);
+		return 0;
+	}
+}
