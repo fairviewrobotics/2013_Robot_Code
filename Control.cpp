e7a767a3805e9f9bf08561f99addf635de4ce39d
unknown <pixelgel@gmail.com>
2013-01-23 16:41:00 -0700

    Normalized the control inputs

--- Control.cpp
+++ Control.cpp
@@ -4,17 +4,18 @@ Control::Control() {
 	joystick = new Joystick(1);
 }
 
+// The axes are calibrated because they only go up to 0.6-0.7
 float Control::GetX(Stick stick) {
 	if(stick == RIGHT) {
-		return joystick->GetRawAxis(1);
+		return joystick->GetX()/0.65f;
 	} else {
-		return joystick->GetRawAxis(3);
+		return joystick->GetRawAxis(5)/0.65f;
 	}
 }
 float Control::GetY(Stick stick) {
 	if(stick == RIGHT) {
-		return joystick->GetRawAxis(2);
+		return joystick->GetY()/0.65f;
 	} else {
-		return joystick->GetRawAxis(4);
+		return joystick->GetRawAxis(3)/0.65f;
 	}
 }
