288e778ac8a1d8e441693c7f93b6f95d7e7044d2
unknown <pixelgel@gmail.com>
2013-01-24 16:54:07 -0700

    Added a deadzone to the TransRot drive

--- TransRotDrive.cpp
+++ TransRotDrive.cpp
@@ -18,17 +18,37 @@ static float absMin(float a, float b) {
 	return abs(a) < abs(b) ? a : b;
 }
 
+static float sign(float n) {
+	return n < 0 ? -1 : 1;
+}
+
 void TransRotDrive::Move(float speed) {
 	goalSpeed = speed;
 }
 
 void TransRotDrive::Rotate(float speed, Rotation dir) {
-	assert(speed >= 0.0f);
-	goalRotationSpeed = speed;
 	direction = dir;
+	goalRotationSpeed = speed;
 }
 
 void TransRotDrive::Update() {
+	// Adding a deadzone
+	const float deadzone = 0.1f;
+	
+	if(goalSpeed < deadzone && goalSpeed > -deadzone) {
+		goalSpeed = 0.0f;
+	} else {
+		goalSpeed += sign(goalSpeed)*0.1f;
+		goalSpeed /= 1.1f;
+	}
+	
+	if(goalRotationSpeed < deadzone && goalRotationSpeed > -deadzone) {
+		goalRotationSpeed = 0.0f;
+	} else {
+		goalRotationSpeed += sign(goalSpeed)*0.1f;
+		goalRotationSpeed /= 1.1f;
+	}
+	
 	// Forwards
 	if(goalSpeed > 0.0f) {
 		if(direction == CW) {
@@ -48,11 +68,11 @@ void TransRotDrive::Update() {
 		if(direction == CW) {
 			leftFrontJaguar->SetSpeed(goalSpeed + goalRotationSpeed);
 			leftBackJaguar->SetSpeed(goalSpeed + goalRotationSpeed);
-			rightFrontJaguar->SetSpeed(absMax(goalSpeed, goalRotationSpeed));
-			rightBackJaguar->SetSpeed(absMax(goalSpeed, goalRotationSpeed));
+			rightFrontJaguar->SetSpeed(absMax(goalSpeed, -goalRotationSpeed));
+			rightBackJaguar->SetSpeed(absMax(goalSpeed, -goalRotationSpeed));
 		} else {
-			leftFrontJaguar->SetSpeed(absMax(goalSpeed, goalRotationSpeed));
-			leftBackJaguar->SetSpeed(absMax(goalSpeed, goalRotationSpeed));
+			leftFrontJaguar->SetSpeed(absMax(goalSpeed, -goalRotationSpeed));
+			leftBackJaguar->SetSpeed(absMax(goalSpeed, -goalRotationSpeed));
 			rightFrontJaguar->SetSpeed(goalSpeed + goalRotationSpeed);
 			rightBackJaguar->SetSpeed(goalSpeed + goalRotationSpeed);
 		}
